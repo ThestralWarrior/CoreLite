91de16eebc06f8716d3e1ed2ec7e59b238ccb88e
ThestralWarrior <ashbroezio@outlook.com>
2024-11-04 01:35:54 +0530

    add memory_set function

--- libc/mem.c
+++ libc/mem.c
@@ -1,7 +1,12 @@
 #include "mem.h"
 
-void memory_copy(char* source, char* dest, int no_of_bytes) {
+void memory_copy(u8* source, u8* dest, int no_of_bytes) {
     for(int i = 0; i < no_of_bytes; i++) {
         *(dest + i) = *(source + i);
     }
+}
+
+void memory_set(u8* dest, u8 val, u32 length) {
+    u8* temp = (u8*) dest;
+    for(; len > 0; len--) *temp++ = val;
 }
--- libc/mem.h
+++ libc/mem.h
@@ -1,6 +1,9 @@
 #ifndef MEM_H
 #define MEM_H
 
-void memory_copy(char* source, char* dest, int no_of_bytes);
+#include "../cpu/types.h"
+
+void memory_copy(u8* source, u8* dest, int no_of_bytes);
+void memory_set(u8* dest, u8 val, u32 length);
 
 #endif
